84a6317768c6d642b18bc2adb5db278c2dcce183
griftt <1361255708@qq.com>
2019-06-18 18:30:46 +0800

    add redis

--- src/main/java/com/griftt/user/controller/LoginController.java
+++ src/main/java/com/griftt/user/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.griftt.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +17,14 @@ public class LoginController {
 
     @RequestMapping("consumer")
     public String login(){
-        stringRedisTemplate.opsForList().set("hello",1,"k2");
-        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-        ops.set("hello2","k3");
-        System.err.println(ops);
-        stringRedisTemplate.opsForList().getOperations().keys("hello");
-        String hello2 = stringRedisTemplate.opsForValue().get("hello2");
-        return hello2;
+
+        //ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        //ops.set("hello2","k3");
+        //stringRedisTemplate.opsForList().getOperations().keys("hello2");
+        //String hello2 = stringRedisTemplate.opsForValue().get("hello2");
+        //System.err.println(hello2);
+        stringRedisTemplate.opsForList().set("k",2,"list");
+        return "ok";
     }
 
 
